846a33f79081f9bf0678d39d183f5f1244f9f6d4
Juan Manuel Mougan <juanmougan@gmail.com>
2017-03-01 22:43:54 -0300

    Added a route for Estates

--- estate.go
+++ estate.go
@@ -2,7 +2,6 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
 
@@ -13,9 +12,22 @@ func main() {
 
     router := mux.NewRouter().StrictSlash(true)
     router.HandleFunc("/", Index)
+    router.HandleFunc("/estates", EstateIndex)
+    router.HandleFunc("/estates/{estateId}", EstateShow)
+
     log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func EstateIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Estate Index!")
+}
+
+func EstateShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    estateId := vars["estateId"]
+    fmt.Fprintln(w, "Estate show:", estateId)
 }
